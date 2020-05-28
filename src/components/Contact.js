89fdbd7ccd6f002e658776062b944af67a4fa08d
Hector Hernandez <hectorohernz@gmail.com>
2020-05-28 17:57:50 -0400

    adding contact page with style WIP

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,9 +6,29 @@ export default class Contact extends Component{
         
     }
     render() {
+        // ADD FUNCTION WITH STATE AND VERFECTION
         return (
             <section className="contact-container">
+                <div className="contact-directions">
+            
+                </div>
+                <div className="contact-form">
+                    <h1 className="contact--header">Send Us A Message</h1>
+
+                    <form className="form-container">
+                    <label for="fname">First And Last name</label>
+                    <input type="text" id="fname" name="fname" placeholder=" Enter Full Name" required/>
+                    
+                    <label for="contact-email">Email</label>
+                    <input type="email" id="contact-email" name="contact-name" placeholder="Enter Email" required/>
+
+                    <label for="contact-textarea">Message</label>
+                    <textarea id="contact-textarea" placeholder="Enter Message Here"></textarea>
+                    
+                    <input type="submit" value="Send Message" id="submit-btn"/>
+                  </form>
                 
+                </div>
             </section>
         )
     }
